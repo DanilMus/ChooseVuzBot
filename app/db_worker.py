ee885c6c62e95afeda4ccbd7d773fe01a293ad4d
Danil <musikhin.danil@gmail.com>
2022-01-23 20:22:48 +0500

    One more change for better work

--- app/db_worker.py
+++ app/db_worker.py
@@ -1,6 +1,9 @@
 import json
 import os 
 
+if not(os.path.exists('data')):
+    os.mkdir('data')
+
 if not(os.path.exists('data/data.json')):
     with open('data/data.json','w',encoding='utf-8') as file:
         json.dump({}, file, indent= 4, ensure_ascii= False)
